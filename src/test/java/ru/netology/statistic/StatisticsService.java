283e888d5946e7c982fe4978c862c69be5fa1ca8
Dimas test <khaerbash@gmail.com>
2024-08-28 15:41:41 +0300

    Initial commit

---
+++ src/test/java/ru/netology/statistic/StatisticsService.java
@@ -0,0 +1,11 @@
+package ru.netology.statistic;
+
+public class StatisticsService {
+    public long findMax(long[] incomesInBillions) {
+        return 12;
+    }
+
+    public long findMin(long[] incomesInBillions) {
+        return 3;
+    }
+}
